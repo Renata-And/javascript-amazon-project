94181a11775074157ff6c530fc1830c824fefbd7
renata-and <renata.androsova@gmail.com>
2024-05-07 16:53:18 +0700

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStorage;
 
-if (!cart) {
-  cart = [
-    {
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-      deliveryOptionId: '1',
-    },
-    {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1,
-      deliveryOptionId: '2',
-    },
-  ];
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1',
+      },
+      {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2',
+      },
+    ];
+  }
 }
 
 function saveToStorage() {
@@ -31,9 +36,13 @@ function getProductFromCart(productId) {
 
 export function addToCart(productId) {
   const matchingItem = getProductFromCart(productId);
-  const quantity = Number(
-    document.querySelector(`.js-quantity-selector-${productId}`).value
-  );
+  // const quantity = Number(
+  //   document.querySelector(`.js-quantity-selector-${productId}`).value
+  // );
+
+  const quantity = document.querySelector(`.js-quantity-selector-${productId}`)
+    ? Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
+    : 1;
 
   if (matchingItem) {
     matchingItem.quantity += quantity;
